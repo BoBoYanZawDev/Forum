156a257ed63133e83a29ae36a48f068aee1d1062
BoBoYanZawDev <boboyanzaw.dev@gmail.com>
2025-09-09 13:52:28 +0630

    added service commands

---
+++ app/Services/Frontend/ThreadService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Services\Frontend;
+
+class ThreadService
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function list($request)
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function storeData($request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function showData($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function updateData($id, $request)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function deleteData($id)
+    {
+        //
+    }}
